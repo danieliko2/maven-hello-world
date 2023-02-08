0e96250ff8e357eefc3da02c0c456e91e3c22d07
daniel.kohav <danielkohav2@gmail.com>
2023-02-08 17:06:26 +0200

    updated versioning script

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM maven:3.8.7-openjdk-18-slim as BUILDER
 WORKDIR /app
 COPY /myapp/ .
-RUN mvn verify
+RUN mvn versions:set -DnewVersion=$(cat .version)
+# RUN mvn verify
 
-FROM openjdk:8-jre-alpine
-WORKDIR /app
-COPY --from=BUILDER /app/target/myapp* .
-CMD java -jar myapp*
+# FROM openjdk:8-jre-alpine
+# WORKDIR /app
+# COPY --from=BUILDER /app/target/myapp* .
+# CMD java -jar myapp*
--- versioning.sh
+++ versioning.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-cd myapp
-VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
+VERSION=$(curl -s https://registry.hub.docker.com/v2/repositories/danieliko/maven-hw-testings/tags/ | jq '.results[0].name') # get the latest version from my repository
+VERSION=$(python3 -c " 
+VERSION=$VERSION.replace('\"', '') 
+print(VERSION) ") # use python to remove double quotes from version
+echo $VERSION
 NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
-echo $NEXTVERSION > version
-mvn versions:set -DnewVersion=$NEXTVERSION
+echo "$NEXTVERSION" > ./myapp/.version
+
+#VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)') get version from 
