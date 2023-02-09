e0dae0622a1ef9c2c6936a97134b353db95f3d63
daniel.kohav <danielkohav2@gmail.com>
2023-02-09 12:42:00 +0200

    trying out versioning with env

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM maven:3.8.7-openjdk-18-slim as BUILDER
 WORKDIR /app
 COPY /myapp/ .
-RUN mvn versions:set -DnewVersion=$(cat .version)
-
+RUN mvn versions:set -DnewVersion=$VERSION
 RUN mvn verify
 
 FROM openjdk:8-jre-alpine
--- versioning.sh
+++ versioning.sh
@@ -3,8 +3,8 @@ VERSION=$(curl -s https://registry.hub.docker.com/v2/repositories/danieliko/mave
 VERSION=$(python3 -c " 
 VERSION=$VERSION.replace('\"', '') 
 print(VERSION) ") # use python to remove double quotes from version
-echo $VERSION
+# echo $VERSION
 NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
-echo "$NEXTVERSION" > ./myapp/.version
+export NEXTVERSION="$NEXTVERSION"
 
-#VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)') get version from 
+echo "$NEXTVERSION"
