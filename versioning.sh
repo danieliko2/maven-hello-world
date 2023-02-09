dd745c3417ccaaffc171f2d7391a6f17bf2bcb7e
daniel.kohav <danielkohav2@gmail.com>
2023-02-09 17:52:07 +0200

    reverted

--- versioning.sh
+++ versioning.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-VERSION=${{ env.POM_VERSION }}
+VERSION=$(curl -s https://registry.hub.docker.com/v2/repositories/danieliko/maven-hw-testings/tags/ | jq '.results[0].name') # get the latest version from my repository
+VERSION=$(python3 -c " 
+VERSION=$VERSION.replace('\"', '') 
+print(VERSION) ") # use python to remove double quotes from version
+# echo $VERSION
 NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
-${{ env.POM_VERSION=$NEXTVERSION}}
+export NEXTVERSION="$NEXTVERSION"
+
 echo "$NEXTVERSION"
