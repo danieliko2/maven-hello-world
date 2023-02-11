9cc15d4d65d547bc0a4e25f2d2e12512f3d880ec
daniel.kohav <danielkohav2@gmail.com>
2023-02-11 20:00:36 +0200

    Finalizing structure 10

--- versioning.sh
+++ versioning.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-VERSION=$(curl -s https://registry.hub.docker.com/v2/repositories/danieliko/maven-hw-testings/tags/ | jq '.results[0].name') # get the latest version from my repository
-VERSION=$(python3 -c " 
-VERSION=$VERSION.replace('\"', '') 
-print(VERSION) ") # use python to remove double quotes from version
-# echo $VERSION
-NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
-export NEXTVERSION="$NEXTVERSION"
+# VERSION=$(curl -s https://registry.hub.docker.com/v2/repositories/danieliko/maven-hw-testings/tags/ | jq '.results[0].name') # get the latest version from my repository
+# VERSION=$(python3 -c " 
+# VERSION=$VERSION.replace('\"', '') 
+# print(VERSION) ") # use python to remove double quotes from version
+# # echo $VERSION
+# NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+# export NEXTVERSION="$NEXTVERSION"
 
-echo "$NEXTVERSION"
+# echo "$NEXTVERSION"
+VERSION=$(curl -s https://registry.hub.docker.com/v2/repositories/danieliko/mavean-hw-testings/tags/ | jq '.results[0].name')
+if [[ "$VERSION" == null ]]; then
+    echo "YAY"
+    VERSION="1.0.0"
+fi
+echo $VERSION
